ec0ff2c64860b00d3dacd288855da15ffa8a2bd8
rofl0r <retnyg@gmx.net>
2012-09-18 19:53:07 +0200

    add more funcs to libintl.h

--- include/libintl.h
+++ include/libintl.h
@@ -2,10 +2,18 @@
 #define LIBINTL_H
 
 #define gettext(X) (X)
+#define dgettext(dom, X) (X)
 #define dcgettext(dom, X, cat) (X)
+#define ngettext(X, Y, N) ((N == 1) ? X : Y)
+#define dngettext(dom, X, Y, N) ((N == 1) ? X : Y)
+#define dcngettext(dom, X, Y, N, cat) ((N == 1) ? X : Y)
+
 #define gettext_noop(X) (X)
+
+// FIXME these should probably return something
 #define bindtextdomain(X, Y)
 #define textdomain(X)
+#define bind_textdomain_codeset(dom, codeset)
 
 #include <stdio.h>
 #define gettext_printf(args...) printf(args)
