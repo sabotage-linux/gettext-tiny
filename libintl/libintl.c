0f71abdfe515bfc3b846da76571d4dd8a5a75797
rofl0r <retnyg@gmx.net>
2012-12-01 03:59:37 +0100

    more variables to make gnu happy

--- libintl/libintl.c
+++ libintl/libintl.c
@@ -76,3 +76,7 @@ char *bind_textdomain_codeset(const char *domainname, const char *codeset)
 	}
 	return NULL;
 }
+
+/* trick configure tests checking for gnu libintl, as in the copy included in gdb */
+const char *_nl_expand_alias () { return NULL; }
+int _nl_msg_cat_cntr = 0;
