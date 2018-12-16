f65b4e0161fe87bd6ddcb9c633c7b7bf52584c0a
xhe <xw897002528@gmail.com>
2018-12-16 15:42:25 +0800

    poparser: return if the buf is too small if line_len, e.g. inputbytes_left is not 0, that means the output buf is large enough.

--- src/poparser.c
+++ src/poparser.c
@@ -165,6 +165,9 @@ enum po_error poparser_feed_line(struct po_parser *p, char* in, size_t in_len) {
 			if ( (len = iconv(p->cd, &line, &line_len, &x, &len)) == (size_t)-1) 
 				return -po_failed_iconv;
 
+			if (line_len != 0)
+				return -po_failed_iconv;
+
 			x = p->buf;
 		}
 
@@ -223,6 +226,9 @@ enum po_error poparser_feed_line(struct po_parser *p, char* in, size_t in_len) {
 			x = p->buf;
 			len = p->bufsize;
 			if ( (len = iconv(p->cd, &line, &line_len, &x, &len)) == (size_t)-1) 
+				return -po_failed_iconv;
+
+			if (line_len != 0)
 				return -po_failed_iconv;
 
 			x = p->buf;
